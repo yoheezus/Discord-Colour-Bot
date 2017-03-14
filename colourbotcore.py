39d58b539299b453dfcf53084557752826c7327c
yoheezus <yoheezus@users.noreply.github.com>
2017-03-14 15:19:10 +0100

    First upload of colourbotCORE.py

---
+++ colourbotcore.py
@@ -0,0 +1,48 @@
+import json
+import discord
+from discord.ext import commands
+
+description = '''A bot that allows users to choose their own name colour through use of a custom role based on id.'''
+
+
+bot = commands.Bot(command_prefix='?', description=description)
+
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+
+
+@bot.event
+async def on_member_remove(member):
+    role = discord.utils.get(member.server.roles, name="ColourID"+member.id)
+    await bot.delete_role(member.server, role)
+
+
+@bot.command(aliases=['nickcolor', 'nc'], pass_context=True)
+async def nickcolour(ctx, value: discord.Colour):
+    userauthor = ctx.message.author.id
+    dserver = ctx.message.server
+    if not discord.utils.get(dserver.roles, name="ColourID"+userauthor):
+        try:
+            role = await bot.create_role(dserver, name="ColourID"+userauthor, colour=value)
+            await bot.add_roles(ctx.message.author, role)
+        except PermissionError:
+            bot.say('Colourbot cannot create/edit roles.')
+    else:
+        try:
+            role = discord.utils.get(dserver.roles, name="ColourID"+userauthor)
+            await bot.add_roles(ctx.message.author, role)
+            await bot.edit_role(dserver, role, colour=value)
+        except PermissionError:
+            bot.say('Colourbot cannot create/edit roles.')
+
+try:
+    bot.run('TOKEN HERE')
+except KeyboardInterrupt:
+    bot.stop()
+finally:
+    bot.close()
